8b2f3261478bd435354fc061d84134fa9eca0355
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-12 10:42:56 +0100

    Rails cookbook - validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :category
   belongs_to :recipe
+
+  validates :recipe_id, uniqueness: { scope: :category_id, message: "Sorry! It's already been added to this list" }
+  validates :comment, length: { minimum: 6 }
 end
